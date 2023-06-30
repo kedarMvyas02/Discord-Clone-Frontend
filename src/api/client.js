b2efbbd0150605f44c508d4edd22c13e56110a39
Kedar Vyas <kedarvyas02@gmail.com>
2023-06-30 17:18:54 +0530

    changed base url of render.com

--- src/api/client.js
+++ src/api/client.js
@@ -3,7 +3,8 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const client = axios.create({
-  baseURL: process.env.API_URL || "http://127.0.0.1:8000",
+  baseURL:
+    "https://discordclonekedar.onrender.com/" || "http://127.0.0.1:8000/",
   withCredentials: true,
 });
 
